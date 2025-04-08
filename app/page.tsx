27c46b6001d4c19ee044cb18a10b473520234b64
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-04-07 21:47:43 -0600

    btn

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Page() {
         <div className="flex space-x-3">
           <Link
             href="/protected"
-            className="rounded-full border-green-300 border  transition-colors hover:bg-green-500 dark:bg-green-700 text-sm sm:text-base h-8 w-full sm:h-10 px-2 sm:px-5 m-2"
+            className="flex h-10 w-full items-center justify-center border transition-all focus:outline-none rounded-full border-green-300 hover:bg-green-500 dark:bg-green-700 text-sm sm:text-base sm:h-10 px-2 sm:px-5 m-2"
           >
             Inicio de sesión
           </Link>
