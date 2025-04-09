26e63512f69b67aa40d55e1f8a3d2da8ab11b38b
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-04-09 11:36:43 -0600

    asd

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,22 +4,7 @@ import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import './globals.css';
 
-import { GeistSans } from 'geist/font/sans';
 
-let title = 'Next.js + Postgres Auth Starter';
-let description =
-  'This is a Next.js starter kit that uses NextAuth.js for simple email + password login and a Postgres database to persist the data.';
-
-export const metadata = {
-  title,
-  description,
-  twitter: {
-    card: 'summary_large_image',
-    title,
-    description,
-  },
-  metadataBase: new URL('https://nextjs-postgres-auth.vercel.app'),
-};
 
 export default function RootLayout({
   children,
@@ -27,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="es">
       <body>
         <ThemeProvider theme={baselightTheme}>
           {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
---
+++ app/metadata.ts
@@ -0,0 +1,12 @@
+export const metadata = {
+    title: 'Carrizales Diaz',
+    description:
+      'Contadores Carrizales Diaz',
+    twitter: {
+      card: 'summary_large_image',
+      title: 'Contadores Carrizales Diaz',
+      description:
+        'Contadores Carrizales Diaz',
+    },
+    metadataBase: new URL('https://taskcount-c-diaz.vercel.app'),
+  };
