23de66b19c1d51c57214a6d5ea0f028ab1b1aff6
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-04-15 17:16:00 -0600

    horas

--- app/check_hora.tsx
+++ app/check_hora.tsx
@@ -5,6 +5,9 @@ import { MarcaTemporal } from "./protected/atributos/marca-temporal";
 
 export const CheckHora = () => {
     const hora = new Date().getHours() + ':' + new Date().getMinutes() ;
+
+    const horaok = new Date();
+    let horas = horaok.toLocaleTimeString() 
     
     const [texto, setTexto] = useState('Entrada');
 
@@ -18,12 +21,12 @@ export const CheckHora = () => {
 
     return (
         <div>
-            <h1>Estas checando {texto} La hora actual es: {hora}</h1>
+            <h1>Estas checando {texto} La hora actual es: {horas}</h1>
             <MarcaTemporal/>
             <button
             className="rounded-full border-green-300 border  transition-colors hover:bg-green-500 dark:bg-green-700 text-sm sm:text-base h-8 w-full sm:h-10 px-2 sm:px-5 m-2" 
             onClick={cambio}>
-                {texto}
+                Checar
             </button>
         </div>
     );
