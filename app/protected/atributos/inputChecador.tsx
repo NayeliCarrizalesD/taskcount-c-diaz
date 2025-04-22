7b00e796fbd22f0ee7a566e2b1d257582a4d26da
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-04-22 13:45:37 -0600

    otra prueba

--- app/protected/atributos/inputChecador.tsx
+++ app/protected/atributos/inputChecador.tsx
@@ -1,44 +1,53 @@
-"use client";
+import { auth } from 'app/auth';
+import { getEntradaSalida } from '@/app/schema';
 
-import React, { useState, useEffect } from "react";
-import { auth } from "app/auth";
-import { getEntradaSalida } from "@/app/schema";
+export const InputChecadorUsuario = async () => {
+  let session = await auth();
+  let correo = session?.user?.email;
 
-export const InputChecadorUsuario = () => {
-  const [checador, setChecador] = useState<string>(""); // Estado para el valor del input
+  let usuarios: any[] = [];
+  let checador: string | undefined;
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const session = await auth();
-        const correo = session?.user?.email;
+  const inputValue  = document.getElementById('checador') as HTMLInputElement;
+  
+  if (inputValue === null) {
+    const inputValue = document.createElement('input');  
+    inputValue.value = 'Entrada';
+  } else {
 
-        if (correo) {
-          const usuarios = await getEntradaSalida(correo);
-
-          if (usuarios.length > 0) {
-            setChecador(usuarios[0].checador || "Entrada"); // Asigna el valor del checador
-          } else {
-            setChecador("Entrada"); // Valor por defecto si no hay usuarios
-          }
+  try {
+      correo?.toString();
+      if (correo) {
+        const usuarioResponse = await getEntradaSalida(correo);
+        usuarios = usuarioResponse;
+    
+        if (usuarios.length > 0) {
+            checador = usuarios[0].checador; // Asignar el nivel del primer usuario a la variable
+            
         }
-      } catch (error) {
-        console.error("Error fetching data:", error);
       }
-    };
+    
+    }
+    catch (error) {
+      console.error(error);
+    }
+}
 
-    fetchData();
-  }, []); // Ejecuta solo una vez al montar el componente
+  
+  return ( 
 
-  return (
     <input
-      id="checador"
-      name="checador"
-      value={checador} // Asigna el valor del estado al input
-      required
-      readOnly
-      type="text"
-      className="mt-1 block w-full text-black rounded-full border border-gray-700 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-black focus:outline-none focus:ring-black sm:text-sm"
-    />
-  );
+    id="checador"
+    name="checador"
+    value={checador}
+    required
+    readOnly
+    type="text"
+    className="mt-1 block w-full text-black rounded-full border border-gray-700 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-black focus:outline-none focus:ring-black sm:text-sm"
+/>
+   
+);
+
 };
+
+
