a017393b1a9100be7dcced9506247edfa4f73c6e
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-04-22 10:16:05 -0600

    hora

--- app/protected/atributos/hora_entrada_salida.tsx
+++ app/protected/atributos/hora_entrada_salida.tsx
@@ -1,11 +1,9 @@
 
 export const HoraEntradaSalida = () => {
-    const hora = new Date().getHours();  
+    const horaok = new Date();
+    let horas = horaok.toLocaleTimeString() ;
 
-    const minutes = new Date().getMinutes();
-    let horas = hora + ':' + minutes; 
-    
-
+    const hora = new Date().getHours() + ':' + new Date().getMinutes() ;
 
 
     return (
@@ -16,6 +14,13 @@ export const HoraEntradaSalida = () => {
          id="hora_entrada_salida"
          name="hora_entrada_salida"
         value={horas}
+        />
+
+<input 
+        className="mt-1 block w-full text-black rounded-full border border-gray-700 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-black focus:outline-none focus:ring-black sm:text-sm"
+         id="hora_entrada_salida"
+         name="hora_entrada_salida"
+        value={hora}
         />
     
       
