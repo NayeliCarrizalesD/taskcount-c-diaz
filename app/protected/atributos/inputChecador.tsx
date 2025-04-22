3caed0ef1d088e6f7c8be5afdf8e3a34945458d5
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-04-22 13:07:26 -0600

    asd

--- app/protected/atributos/inputChecador.tsx
+++ app/protected/atributos/inputChecador.tsx
@@ -1,49 +1,46 @@
-"use client";
+import { auth } from 'app/auth';
+import { getEntradaSalida } from '@/app/schema';
 
-import React, { useState, useEffect } from "react";
-import { auth } from "app/auth";
-import { getEntradaSalida } from "@/app/schema";
+export const InputChecadorUsuario = async () => {
+  let session = await auth();
+  let correo = session?.user?.email;
 
-export const InputChecadorUsuario = () => {
-  const [checadorValue, setChecadorValue] = useState<string>("Entrada");
+  let usuarios: any[] = [];
+  let checador: string | undefined;
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const session = await auth();
-        const correo = session?.user?.email;
+  
 
-        if (correo) {
-          const usuarios = await getEntradaSalida(correo);
-
-          if (usuarios.length > 0) {
-            const checador = usuarios[0].checador;
-
-            // Actualiza el estado basado en el valor de "checador"
-            if (checador === "Entrada") {
-              setChecadorValue("Salida");
-            } else if (checador === "Salida") {
-              setChecadorValue("Entrada");
-            }
-          }
+  try {
+      correo?.toString();
+      if (correo) {
+        const usuarioResponse = await getEntradaSalida(correo);
+        usuarios = usuarioResponse;
+    
+        if (usuarios.length > 0) {
+            checador = usuarios[0].checador; // Asignar el nivel del primer usuario a la variable
         }
-      } catch (error) {
-        console.error("Error fetching data:", error);
       }
-    };
+    
+    }
+    catch (error) {
+      console.error(error);
+    }
 
-    fetchData();
-  }, []); // Ejecuta solo una vez al montar el componente
+  
+  return ( 
 
-  return (
     <input
-      id="checador"
-      name="checador"
-      value={checadorValue} // Asigna el valor del estado al input
-      required
-      readOnly
-      type="text"
-      className="mt-1 block w-full text-black rounded-full border border-gray-700 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-black focus:outline-none focus:ring-black sm:text-sm"
-    />
-  );
+    id="checador"
+    name="checador"
+    value={checador}
+    required
+    readOnly
+    type="email"
+    className="mt-1 block w-full text-black rounded-full border border-gray-700 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-black focus:outline-none focus:ring-black sm:text-sm"
+/>
+   
+);
+
 };
+
+
