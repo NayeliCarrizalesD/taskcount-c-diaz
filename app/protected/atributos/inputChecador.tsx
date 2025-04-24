3f2f0d77deca689ea40b9c3533204a08a45a17ae
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-04-24 13:59:21 -0600

    texto del input

--- app/protected/atributos/inputChecador.tsx
+++ app/protected/atributos/inputChecador.tsx
@@ -8,6 +8,52 @@ export const InputChecadorUsuario = async () => {
   let lastCheck: any = null;
   let checador: string | undefined;
 
+  try {
+      correo?.toString();
+      if (correo) {
+        const usuarioCheck = await getLastEntradaSalida(correo);
+        lastCheck = usuarioCheck;
+
+        if (lastCheck) {
+            checador = lastCheck.checador; // Asignar el nivel del usuario a la variable
+            if (checador === "Entrada") {
+                checador = "Salida";
+            } else if (checador === "Salida") {
+                checador = "Entrada";
+            } else {
+                checador = "Entrada"; // Valor por defecto si no es ni Entrada ni Salida
+            }
+        }
+      }
+    
+    }
+    catch (error) {
+      console.error(error);
+    }
+
+  
+  return ( 
+    <>
+        <input
+        id="checador"
+        name="checador"
+        required
+        defaultValue="Entrada"
+        value={checador}
+        readOnly
+        type="text"
+        className="mt-1 display:none block w-full text-black rounded-full border border-gray-700 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-black focus:outline-none focus:ring-black sm:text-sm"
+    />
+    </>
+  );
+};
+
+export const TextoInputChecadorUsuario = async () => {
+  let session = await auth();
+  let correo = session?.user?.email;
+
+  let lastCheck: any = null;
+  let checador: string | undefined;
 
   try {
       correo?.toString();
@@ -34,21 +80,11 @@ export const InputChecadorUsuario = async () => {
 
   
   return ( 
-<>
-    <input
-    id="checador"
-    name="checador"
-    required
-    defaultValue="Entrada"
-    value={checador}
-    readOnly
-    type="text"
-    className="mt-1 display:none block w-full text-black rounded-full border border-gray-700 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-black focus:outline-none focus:ring-black sm:text-sm"
-/>
-</>
-   
-);
+    <>
+       {checador}
 
+    </>
+  );
 };
 
 
--- app/protected/atributos/registro_entrada_salida/place_formulario.tsx
+++ app/protected/atributos/registro_entrada_salida/place_formulario.tsx
@@ -5,6 +5,7 @@ import { createNewEntradaSalida, getEntradaSalida } from "@/app/schema";
 import { SubmitButtonFlete } from "@/app/submit_button_flete";
 import { redirect } from "next/navigation";
 import { FiFolder } from "react-icons/fi";
+import { TextoInputChecadorUsuario } from "../inputChecador";
 
 
 export default function FormularioChecarEntrada() {
@@ -35,7 +36,7 @@ export default function FormularioChecarEntrada() {
                     Checador
                 </h3>
                 <h3 className="font-normal">
-                    Registrar Entrada / Salida
+                    Registrar Entrada / Salida <TextoInputChecadorUsuario/>
                 </h3>
             </div>
             <div className="h-[auto] sm:h-auto px-4 flex-row items-center ">
