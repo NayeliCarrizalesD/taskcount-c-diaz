47ae15edaffe25a62c3bf8d3f771ab6fa1305aeb
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-04-25 10:34:21 -0600

    asd

--- app/formCheckHoras.tsx
+++ app/formCheckHoras.tsx
@@ -17,7 +17,7 @@ export function FormCheckHora({
         action={action}
         className="flex flex-col space-y-4 bg-zinc-800 px-2 py-5 sm:px-4"
       >
-        <div className="invisible">
+        <div className="">
           <label
           
             htmlFor="fecha_entrada_salida"
