2d1b585f8cf0901263145f1100c6e7707629a854
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-04-30 12:33:41 -0600

    qwe

--- app/protected/atributos/dashboard/dashboard.tsx
+++ app/protected/atributos/dashboard/dashboard.tsx
@@ -5,7 +5,7 @@ import { Grid } from "./grid";
 
 export function Dashboard() {
     return (
-        <div className="p-4 sm:ml-64bg-zinc-700 rounded-3xl pb-4 shadow h-auto">
+        <div className="p-4 sm:ml-64 bg-zinc-700 rounded-3xl pb-4 shadow h-auto">
             <TopBar/>
             <Grid />
         </div>
