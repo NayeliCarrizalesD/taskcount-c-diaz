796c6af386c8aad8b23baf604712108cbe9515d2
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-04-30 13:25:34 -0600

    qwe

--- app/protected/atributos/dashboard/dashboard.tsx
+++ app/protected/atributos/dashboard/dashboard.tsx
@@ -5,14 +5,9 @@ import { Grid } from "./grid";
 
 export function Dashboard() {
     return (
-        <div className="p-4 sm:ml-64 bg-zinc-700 rounded-3xl pb-4 shadow h-auto">
-            <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
-            <div className="grid grid-cols-3 gap-4 mb-4">
-            
+        <div className="p-4 sm:ml-64 gap-4 bg-zinc-700 rounded-3xl pb-4 shadow h-auto">
             <TopBar/>
             <Grid />
-            </div>
-            </div>
         </div>
     );
 };
