1ab7e121595addcbd4fe14b607a0266e2cddacea
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-04-30 12:06:36 -0600

    sidebar

--- app/protected/atributos/sidebar/sidebar_admin/sidebar.tsx
+++ app/protected/atributos/sidebar/sidebar_admin/sidebar.tsx
@@ -6,21 +6,15 @@ export function SideBarAdmin(){
 
   return (
     <>
-
-<div className="text-center">
-   <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" type="button" data-drawer-target="drawer-navigation" data-drawer-show="drawer-navigation" aria-controls="drawer-navigation">
-   Show navigation
-   </button>
-</div>
+    
 
 
-<div id="drawer-navigation" className="fixed top-0 left-0 z-40 w-64 h-screen p-4 overflow-y-auto transition-transform -translate-x-full bg-white dark:bg-gray-800" aria-labelledby="drawer-navigation-label">
-    <h5 id="drawer-navigation-label" className="text-base font-semibold text-gray-500 uppercase dark:text-gray-400">Menu</h5>
-    <button type="button" data-drawer-hide="drawer-navigation" aria-controls="drawer-navigation" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 end-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" >
-        <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-        <span className="sr-only">Close menu</span>
-    </button>
-  <div className="py-4 overflow-y-auto">
+<aside id="logo-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+   <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+      <a href="https://flowbite.com/" className="flex items-center ps-2.5 mb-5">
+         <img src="https://flowbite.com/docs/images/logo.svg" className="h-6 me-3 sm:h-7" alt="Flowbite Logo" />
+         <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+      </a>
       <ul className="space-y-2 font-medium">
          <li>
             <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
@@ -85,7 +79,11 @@ export function SideBarAdmin(){
          </li>
       </ul>
    </div>
-</div>
+</aside>
+
+
+
+   
 
     
     </>
--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -29,8 +29,13 @@ export default async function ProtectedPage() {
   }
 
   return (
-    <main className="grid bg-black text-white gap-4 p-4 grid-cols-[220px,_1fr]">
-
+<main>    
+<button data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar" type="button" className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+   <span className="sr-only">Open sidebar</span>
+   <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+   <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+   </svg>
+</button>
     {nivelUsuario ==='na1' ?<SideBarAdmin/>: "" }
     {nivelUsuario ==='n1' ?<SideBarN1/>: "" }
     {nivelUsuario ==='n2' ?<SideBarN2/>: "" }
@@ -146,7 +151,10 @@ export default async function ProtectedPage() {
 }
 
 /*
- <Dashboard />
+
+    <main className="grid bg-black text-white gap-4 p-4 grid-cols-[220px,_1fr]">
+
+<Dashboard />
 {usuarios.map((usuario) => (
           <p key={usuario.id}>Nivel: {usuario.nivel}</p>
         ))}
