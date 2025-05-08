52c1bea75a4d9f672afbff31c2241bf72dd8ed6b
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-05-08 16:29:27 -0600

    estilos

--- app/protected/atributos/sidebar/sidebar_admin/sidebar.tsx
+++ app/protected/atributos/sidebar/sidebar_admin/sidebar.tsx
@@ -15,7 +15,7 @@ export function SideBarAdmin(){
 </button>
 
 <aside id="logo-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-   <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+   <div className="h-full px-3 py-4 overflow-y-auto bg-black dark:bg-black">
      <AccountAdminToggle/>
      <RouteSelectAdmin/> 
      <Plan/>
