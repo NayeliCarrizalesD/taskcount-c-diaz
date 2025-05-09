bb4c0b10ede324d6922762b4f8a1f1b58baf1cb1
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-05-09 14:00:19 -0600

    asd

--- app/protected/atributos/registro_productos/registro_productos.tsx
+++ app/protected/atributos/registro_productos/registro_productos.tsx
@@ -4,7 +4,7 @@ import { GridRegistroProductos } from "./grid_registro_productos";
 
 export function RegistroProductos() {
     return (
-        <div className="bg-zinc-700 rounded-3xl pb-4 shadow h-auto">
+        <div className="p-4 sm:ml-64 m-3 bg-zinc-700 rounded-3xl pb-4 shadow h-auto">
             <TopBar/>
             <GridRegistroProductos />
         </div>
