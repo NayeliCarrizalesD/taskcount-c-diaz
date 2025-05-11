8aba19df22866570083a416b74353803db98db73
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-05-11 06:13:52 -0600

    menu lateral

--- app/protected/atributos/sidebar/account_Admin_Toggle.tsx
+++ app/protected/atributos/sidebar/account_Admin_Toggle.tsx
@@ -26,10 +26,12 @@ export const AccountAdminToggle = async () => {
         <>
        
             
-              <div className="text-center px-2">
+              <div className="flex items-center p-2 text-gray-900 rounded-full dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                   <span className="text-sm font-bold block">
                       {nameUsuario} 
                   </span> 
+              </div>    
+              <div className="flex items-center p-2 text-gray-900 rounded-full dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                   
                   <span className="text-xs block py-1">
                       {session?.user?.email}
