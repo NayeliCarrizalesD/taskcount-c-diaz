e9e941230619f85bed7962091f1dc6a402251662
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-05-11 01:32:16 -0600

    cotizador

--- app/protected/atributos/cotizador/cotizador.tsx
+++ app/protected/atributos/cotizador/cotizador.tsx
@@ -3,7 +3,7 @@ import { GridCotizador } from "./grid_cotizador";
 
 export function Cotizador() {
     return(
-        <div className="bg-zinc-700 rounded-3xl pb-4 shadow h-auto">
+        <div className="p-4 sm:ml-64 m-3 bg-zinc-700 rounded-3xl pb-4 shadow h-auto">
             <TopBar/>
             <GridCotizador/>    
         </div>
