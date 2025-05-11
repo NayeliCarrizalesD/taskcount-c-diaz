06ca567cb00fbe3f5f7feff39a5deb7b1f3a2cb5
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-05-11 01:33:50 -0600

    prueba placeholder

--- app/protected/atributos/catalogo_clientes/placeholder_catalogo_clientes.tsx
+++ app/protected/atributos/catalogo_clientes/placeholder_catalogo_clientes.tsx
@@ -3,7 +3,7 @@ import TablaClientes from "./tabla_clientes";
 export async function PlaceholderTablaClientes() {
 
     return (
-        <div className="lg:col-span-12 sm:col-span-12 rounded-3xl bg-zinc-800 border border-stone-500 shadow-lg h-[auto] sm:h-auto overflow-y-scroll scrollbar-thin">
+        <div className="lg:col-span-8 sm:col-span-12 overflow-hidden rounded-3xl bg-zinc-800 border border-stone-500 shadow-lg">
             <div className="p-4">
                 <h3 className="flex items-center text-lg gap-1.5 font-medium">
                     Productos
