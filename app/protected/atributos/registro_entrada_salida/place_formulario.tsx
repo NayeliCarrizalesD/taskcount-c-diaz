3a340c41fa89b30c81b6ef3ab284b39e21436397
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-05-11 06:03:40 -0600

    slate prueba de color

--- app/protected/atributos/registro_entrada_salida/place_formulario.tsx
+++ app/protected/atributos/registro_entrada_salida/place_formulario.tsx
@@ -31,7 +31,7 @@ export default function FormularioChecarEntrada() {
     }
 
     return (
-        <div className="lg:col-span-4 sm:col-span-12 rounded-3xl bg-zinc-800 border border-stone-500 shadow-lg h-[auto] sm:h-auto overflow-y-scroll scrollbar-thin">
+        <div className="lg:col-span-4 sm:col-span-12 rounded-3xl bg-slate-800 border border-stone-500 shadow-lg h-[auto] sm:h-auto overflow-y-scroll scrollbar-thin">
             <div className="p-4">
                 <h3 className="flex items-center text-lg gap-1.5 font-medium">
                     <FiFolder />
