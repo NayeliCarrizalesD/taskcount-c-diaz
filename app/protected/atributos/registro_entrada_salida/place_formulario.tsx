3d0eb3fb0eb7618a67ed1a3fc67d5debe80605c1
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-05-11 06:11:06 -0600

    asd

--- app/protected/atributos/registro_entrada_salida/place_formulario.tsx
+++ app/protected/atributos/registro_entrada_salida/place_formulario.tsx
@@ -40,12 +40,6 @@ export default function FormularioChecarEntrada() {
                 <h3 className="font-normal">
                     Hola <TextoInputNombreUsuario/> se esta registrando <TextoInputChecadorUsuario/> 
                 </h3>
-                <h3 className="font-normal">
-                    El dia: <TextoFechaEntradaSalida/>
-                </h3>
-                <h3 className="font-normal">
-                    Hora: <TextoHoraEntradaSalida/>
-                </h3>
             </div>
             <div className="h-[auto] sm:h-auto px-4 flex-row items-center ">
                 <FormCheckHora action={EntradaSalida} >
