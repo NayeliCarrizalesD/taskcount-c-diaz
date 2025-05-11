1358b291672a72fc1e10ed7d6b534d000489636d
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-05-11 06:31:24 -0600

    estilo del placeholder

--- app/protected/atributos/check_entrada_salida/placeholder_check.tsx
+++ app/protected/atributos/check_entrada_salida/placeholder_check.tsx
@@ -4,7 +4,7 @@ import FormularioChecarEntrada from "../registro_entrada_salida/place_formulario
 
 export const PlaceholderCheckHora = () => {
     return (
-        <div className="lg:col-span-8 sm:col-span-12 overflow-hidden rounded-3xl bg-slate-800 border border-stone-500 shadow-lg">
+        <div className="lg:col-span-8 sm:col-span-12 overflow-hidden rounded-3xl bg-slate-800 border border-stone-500 shadow-lg h-[auto] sm:h-auto overflow-y-scroll scrollbar-thin">
             <div className="p-4">
                 <h3 className="flex items-center gap-1.5 font-medium">
                     Hora
