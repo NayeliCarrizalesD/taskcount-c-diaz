d74fc3228da48c2f4289401988bc85db45de9edd
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-05-14 13:42:18 -0600

    asd

--- app/protected/atributos/configuracion_clientes_honorarios/tabla_clientes_honorarios.tsx
+++ app/protected/atributos/configuracion_clientes_honorarios/tabla_clientes_honorarios.tsx
@@ -18,15 +18,17 @@ export default async function TablaClienteHonorarios() {
             <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400w-full bg-neutral-800  dark:text-gray-400 table-auto min-w-max">   
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
-                        <th className='p-4 border-b uppercase border-neutral-500 text-slate-100 bg-zinc-900'>Nombre del concepto</th>
-                        <th className='p-4 border-b uppercase border-neutral-500 text-slate-100 bg-zinc-900'>Registrado por:</th>
+                        <th className='p-4 border-b uppercase border-neutral-500 text-slate-100 bg-zinc-900'>Cliente</th>
+                        <th className='p-4 border-b uppercase border-neutral-500 text-slate-100 bg-zinc-900'>Concepto</th>
+                        <th className='p-4 border-b uppercase border-neutral-500 text-slate-100 bg-zinc-900'>Cantidad a pagar</th>
                     </tr>
                 </thead>
                 <tbody>
                     {productos && productos.map((producto: any, index: number) => (
                         <tr className={index % 2 ? "bg-stone-700 text-sm hover:bg-black hover:text-white border-b border-neutral-500" : "text-sm hover:bg-black hover:text-white border-b border-neutral-500"} key={producto.id}>
-                            <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{producto.nombre_producto_servicio}</td>
-                            <td className="p-4">{producto.correo_empleado}</td>
+                            <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{producto.nombre_cliente}</td>
+                            <td className="p-4">{producto.concepto}</td>
+                            <td className="p-4">$ {producto.pago}</td>
                         </tr>
                     ))}
                 </tbody>
