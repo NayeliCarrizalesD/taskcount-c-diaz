f7685d0f03690ae894f4c1670e5f8b12322e142d
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-05-14 11:52:34 -0600

    dedtalles finales

--- app/protected/atributos/configuracion_clientes_honorarios/placeholder_form_clientes_honorarios.tsx
+++ app/protected/atributos/configuracion_clientes_honorarios/placeholder_form_clientes_honorarios.tsx
@@ -21,7 +21,7 @@ export default function FormularioClientesHonorarios() {
         return console.log('La configuracion ya existe');              
     } else {
         await createCosto(nombre_cliente, concepto, pago);
-        redirect('/protected/catalogo_productos'); // Redirigir a la página de registro de productos
+        redirect('/protected/config_clientes_honorarios'); // Redirigir a la página de registro de productos
     }             
     }
 
