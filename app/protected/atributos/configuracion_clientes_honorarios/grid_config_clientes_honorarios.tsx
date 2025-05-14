de742a72a511a29baea48262b08c51a3fdb9779f
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-05-14 13:23:47 -0600

    asd

--- app/protected/atributos/configuracion_clientes_honorarios/grid_config_clientes_honorarios.tsx
+++ app/protected/atributos/configuracion_clientes_honorarios/grid_config_clientes_honorarios.tsx
@@ -1,12 +1,13 @@
 import Footer from "../footer";
 import FormularioClientesHonorarios from "./placeholder_form_clientes_honorarios";
-
+import { PlaceholderTablaclientesHonorarios } from "./placeholder_tabla_clientes_honorarios";
 
 export const GridConfigClientesHonorarios = () => {
     return (
         <>
             <div className="px-4 grid gap-3 grid-cols-12">
             <FormularioClientesHonorarios/>
+            <PlaceholderTablaclientesHonorarios/>
             </div>
             <Footer />
         </>
---
+++ app/protected/atributos/configuracion_clientes_honorarios/placeholder_tabla_clientes_honorarios.tsx
@@ -0,0 +1,20 @@
+import TablaClienteHonorarios from "./tabla_clientes_honorarios";
+
+export async function PlaceholderTablaclientesHonorarios() {
+
+    return (
+        <div className="lg:col-span-8 sm:col-span-12 overflow-hidden rounded-3xl bg-slate-800 border border-stone-500 shadow-lg h-[auto] sm:h-auto overflow-y-scroll scrollbar-thin">
+            <div className="p-4">
+                <h3 className="flex items-center text-lg gap-1.5 font-medium">
+                    Conceptos
+                </h3>
+                <h3 className="font-normal">
+                    Listado de conceptos de servicios y productos.
+                </h3>
+            </div>
+            <div className="h-[auto] sm:h-auto px-4 flex-row items-center ">
+             <TablaClienteHonorarios/>
+            </div>
+        </div>
+    )
+}
