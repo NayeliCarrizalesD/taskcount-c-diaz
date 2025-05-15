118d545594b0f97e1ccf2d1f9bf9b06b5fd83d92
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-05-15 16:31:12 -0600

    actualizacion de estilos

--- app/protected/atributos/configuracion_clientes_honorarios/config_clientes_honorarios.tsx
+++ app/protected/atributos/configuracion_clientes_honorarios/config_clientes_honorarios.tsx
@@ -6,7 +6,7 @@ import { GridConfigClientesHonorarios } from "./grid_config_clientes_honorarios"
 
 export function ConfigClientesHonorarios() {
     return (
-        <div className="p-4 sm:ml-64 m-3 bg-zinc-700 rounded-3xl pb-4 shadow h-auto">
+        <div className="p-4 sm:ml-64 m-3 bg-zinc-800 rounded-3xl pb-4 shadow h-auto">
             <TopBar/>
             <GridConfigClientesHonorarios />
         </div>
--- app/protected/atributos/tablaChecador/checador.tsx
+++ app/protected/atributos/tablaChecador/checador.tsx
@@ -6,7 +6,7 @@ import { GridTablaFullChecador} from "./grid_tabla_checador";
 
 export function ConsultaChecador() {
     return (
-        <div className="bg-zinc-700 rounded-3xl pb-4 shadow h-auto">
+        <div className="p-4 sm:ml-64 m-3 bg-zinc-800 rounded-3xl pb-4 shadow h-auto">
             <TopBar/>
             <GridTablaFullChecador />
         </div>
