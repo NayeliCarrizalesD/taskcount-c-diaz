da126a9dda147fc3d11b88e17158fa29d8a4aea1
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-07-08 10:18:05 -0600

    fondo

--- app/protected/atributos/tablaChecador/tabla_checador_placeholder.tsx
+++ app/protected/atributos/tablaChecador/tabla_checador_placeholder.tsx
@@ -3,7 +3,7 @@ import TablaChecador from "./tabla_checador";
 export async function TablaChecadorFull() {
 
     return (
-        <div className="lg:col-span-12 sm:col-span-12 rounded-3xl bg-zinc-800 border border-stone-500 shadow-lg h-[auto] sm:h-auto overflow-y-scroll scrollbar-thin">
+        <div className="lg:col-span-12 sm:col-span-12 rounded-3xl bg-zinc-900 border shadow-lg h-[auto] sm:h-auto overflow-y-scroll scrollbar-thin">
             <div className="p-4">
                 <h3 className="flex items-center text-lg gap-1.5 font-medium">
                     Entradas / Salidas
