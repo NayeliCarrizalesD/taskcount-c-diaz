d0d81adb7f514d17aebdf85e836cd540ea47cfc2
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-07-09 13:27:47 -0600

    columnas

--- app/protected/atributos/registro_clientes/place_catalogo_clientes_min.tsx
+++ app/protected/atributos/registro_clientes/place_catalogo_clientes_min.tsx
@@ -4,7 +4,7 @@ import TablaClientes from "../catalogo_clientes/tabla_clientes";
 export async function PlaceholderTablaClientesPequenio() {
 
     return (
-        <div className="lg:col-span-7 sm:col-span-12 overflow-hidden rounded-3xl bg-zinc-800 shadow-xl h-[600px] sm:h-auto overflow-y-scroll scrollbar-thin">
+        <div className="lg:col-span-8 sm:col-span-12 overflow-hidden rounded-3xl bg-zinc-800 shadow-xl h-[600px] sm:h-auto overflow-y-scroll scrollbar-thin">
             <div className="p-4">
                 <h3 className="flex items-center text-lg gap-1.5 font-medium">
                     Clientes
