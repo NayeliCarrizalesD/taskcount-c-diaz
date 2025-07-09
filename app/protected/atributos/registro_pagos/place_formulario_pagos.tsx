0a68a173d33191e39664b1ccf1c33ee57786c501
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-07-09 12:41:35 -0600

    actualizacion

--- app/protected/atributos/registro_pagos/place_formulario_pagos.tsx
+++ app/protected/atributos/registro_pagos/place_formulario_pagos.tsx
@@ -33,10 +33,10 @@ export default function FormularioRegistroPagoHonorarios() {
             <div className="p-4">
                 <h3 className="flex items-center text-lg gap-1.5 font-medium">
                     <AiOutlineTag />
-                    PAgo honorarios 
+                    Pago Honorarios 
                 </h3>
                 <h3 className="font-normal">
-                    Registrar un concepto en el catalogo de productos y servicios
+                    Registrar un pago de honorarios a un cliente de años pasados
                 </h3>
             </div>
             <div className="h-[auto] sm:h-auto px-4 flex-row items-center ">
