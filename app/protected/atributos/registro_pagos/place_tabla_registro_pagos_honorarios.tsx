bd9454b49174209ed03746d93be1af6b816a7a54
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-07-09 13:21:47 -0600

    estilos

--- app/protected/atributos/registro_pagos/place_tabla_registro_pagos_honorarios.tsx
+++ app/protected/atributos/registro_pagos/place_tabla_registro_pagos_honorarios.tsx
@@ -4,7 +4,7 @@ import TablaPagosHonorarios from "./tabla_resgitro_pago";
 export async function PlaceholderTablaPagosHonorarios() {
 
     return (
-        <div className="lg:col-span-8 sm:col-span-12 overflow-hidden rounded-3xl bg-slate-800 shadow-xl h-[auto] sm:h-auto overflow-y-scroll scrollbar-thin">
+        <div className="lg:col-span-8 sm:col-span-12 overflow-hidden rounded-3xl bg-zinc-800 shadow-xl h-[auto] sm:h-auto overflow-y-scroll scrollbar-thin">
             <div className="p-4">
                 <h3 className="flex items-center text-lg gap-1.5 font-medium">
                     Pagos Honorarios
