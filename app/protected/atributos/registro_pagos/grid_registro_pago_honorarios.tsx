f449b7c9a4062cb601085d9ebe15c92b0d3b4daa
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-07-09 13:14:39 -0600

    tabla

--- app/protected/atributos/registro_pagos/grid_registro_pago_honorarios.tsx
+++ app/protected/atributos/registro_pagos/grid_registro_pago_honorarios.tsx
@@ -1,5 +1,6 @@
 import Footer from "../footer";
 import FormularioRegistroPagoHonorarios from "./place_formulario_pagos";
+import { PlaceholderTablaPagosHonorarios } from "./place_tabla_registro_pagos_honorarios";
 
 
 
@@ -8,6 +9,7 @@ export const GridRegistroPagoHonorarios = () => {
         <>
             <div className="px-4 grid gap-3 grid-cols-12">
                 <FormularioRegistroPagoHonorarios />
+                <PlaceholderTablaPagosHonorarios />
         
             </div>
             <Footer />
--- app/protected/atributos/registro_pagos/place_tabla_registro_pagos_honorarios.tsx
+++ app/protected/atributos/registro_pagos/place_tabla_registro_pagos_honorarios.tsx
@@ -1,7 +1,7 @@
 import TablaPagosHonorarios from "./tabla_resgitro_pago";
 
 
-export async function PlaceholderTablaDatosUsuario() {
+export async function PlaceholderTablaPagosHonorarios() {
 
     return (
         <div className="lg:col-span-8 sm:col-span-12 overflow-hidden rounded-3xl bg-slate-800 shadow-xl h-[auto] sm:h-auto overflow-y-scroll scrollbar-thin">
