a6bf07b7e36aa648564da33c6597eafd884e258c
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-07-10 10:34:10 -0600

    asd

--- app/protected/atributos/registro_datos_usuario/place_tabla_datos_usuario.tsx
+++ app/protected/atributos/registro_datos_usuario/place_tabla_datos_usuario.tsx
@@ -1,4 +1,4 @@
-import { SelectClienteOnChange } from "../selectClienteFuncion";
+
 import TablaDatosUsuarios from "./tabla_datos_usuario";
 
 export async function PlaceholderTablaDatosUsuario() {
@@ -14,7 +14,7 @@ export async function PlaceholderTablaDatosUsuario() {
                 </h3>
             </div>
             <div className="h-[auto] sm:h-auto px-4 flex-row items-center ">
-                <SelectClienteOnChange/>
+               
              <TablaDatosUsuarios />
             </div>
         </div>
