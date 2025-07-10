6f634231b9494e8a19c0f51c605135738743f60b
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-07-10 10:31:43 -0600

    cliente

--- app/protected/atributos/selectClienteFuncion.tsx
+++ app/protected/atributos/selectClienteFuncion.tsx
@@ -3,6 +3,11 @@ import {  getTodosClientes } from '@/app/schema';
 
 export const SelectClienteOnChange = async () => {
   let clienteNombre: any[] = [];
+  let clienteSelecionado = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedCliente = e.target.value; 
+    console.log("Cliente seleccionado:", selectedCliente);
+    // Aquí puedes manejar el cambio, por ejemplo, actualizar el estado o hacer una llamada a la API
+  };
   
     try {
       clienteNombre = await getTodosClientes();      
@@ -10,10 +15,13 @@ export const SelectClienteOnChange = async () => {
       catch (error) {
         console.error(error);
       }
+
+
     return ( 
       <select
       id="nombre_cliente"
       name="nombre_cliente"
+      onChange={clienteSelecionado}
       required
       className="mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
       >
