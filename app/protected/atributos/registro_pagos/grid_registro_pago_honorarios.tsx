bcd0324108ca61e4a66987abdfd0467d7a69c6e5
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-07-14 12:57:53 -0600

    tercer intento

--- app/protected/atributos/registro_pagos/grid_registro_pago_honorarios.tsx
+++ app/protected/atributos/registro_pagos/grid_registro_pago_honorarios.tsx
@@ -1,6 +1,6 @@
 import Footer from "../footer";
 import FormularioRegistroPagoHonorarios from "./place_formulario_pagos";
-import { PlaceholderTablaPagosHonorarios } from "./place_tabla_registro_pagos_honorarios";
+import PlaceholderTablaPagosHonorarios  from "./place_tabla_registro_pagos_honorarios";
 
 
 
@@ -9,7 +9,7 @@ export const GridRegistroPagoHonorarios = () => {
         <>
             <div className="px-4 grid gap-3 grid-cols-12">
                 <FormularioRegistroPagoHonorarios />
-                <PlaceholderTablaPagosHonorarios />
+                <PlaceholderTablaPagosHonorarios datosTabla={[]} />
         
             </div>
             <Footer />
