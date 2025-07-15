d8e689ef55309cc56a767ed76cbd0c418384f85b
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-07-15 10:25:03 -0600

    intento 3

--- app/protected/atributos/registro_datos_usuario/place_formulario_datos_usuario.tsx
+++ app/protected/atributos/registro_datos_usuario/place_formulario_datos_usuario.tsx
@@ -1,26 +1,10 @@
 import { FormDatosUsuario } from "@/app/formularios/formDatosUsuario";
 import { SubmitButton } from "@/app/submit-button";
-import { createDatosUsuario, getUsuario } from "@/app/schema";
 import { FiUser } from "react-icons/fi";
-import { redirect } from "next/navigation";
+import { createRegistroPagoHonorarios } from "./funcion_registro_pago";
 
 export default function FormularioDatosUsuario() {
-  async function datosUsuario(formData: FormData) {
-    'use server';
-    let fecha_alta = formData.get('fecha_alta') as unknown as string;
-    let telefono_usuario = formData.get('telefono_usuario') as string;
-    let correo = formData.get('correo') as string;
-    let nivel = formData.get('nivel') as string;
-    let usuario = await getUsuario(correo);
-
-    if (usuario.length > 0) {
-      return console.log('Usuario ya existe');  
-    }
-    else {
-        await createDatosUsuario(fecha_alta, telefono_usuario, correo, nivel);
-        redirect('/ProtectedRegistroDatosUsuarios');
-    }
-    }
+  
     return (
         <div className="lg:col-span-4 sm:col-span-12 overflow-hidden rounded-3xl bg-slate-800 shadow-xl h-[auto] sm:h-auto overflow-y-scroll scrollbar-thin">
             <div className="p-4">
@@ -33,7 +17,7 @@ export default function FormularioDatosUsuario() {
                 </h3>
             </div>
             <div className="h-[auto] sm:h-auto px-4 flex-row items-center ">
-                <FormDatosUsuario action={datosUsuario}>
+                <FormDatosUsuario action={createRegistroPagoHonorarios}>
                     <SubmitButton>Registrar</SubmitButton>
                 </FormDatosUsuario>
             </div>
