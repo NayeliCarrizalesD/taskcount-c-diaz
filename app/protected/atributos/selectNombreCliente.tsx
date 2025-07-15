92b27e1aec58040006bfb7644d108911f16245a1
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-07-15 11:36:28 -0600

    otro cambio a api

--- app/protected/atributos/selectNombreCliente.tsx
+++ app/protected/atributos/selectNombreCliente.tsx
@@ -1,15 +1,21 @@
+"use client";
+import { useEffect, useState } from "react";
 
-import {  getTodosClientes } from '@/app/schema';
+export const SelectNombreClienteTodos = () => {
+  const [clientes, setClientes] = useState<any[]>([]);
 
-export const SelectNombreClienteTodos = async () => {
-  let clienteNombre: any[] = [];
-  
-    try {
-      clienteNombre = await getTodosClientes();      
-      }
-      catch (error) {
-        console.error(error);
-      }
+    useEffect(() => {
+        const fetchClientes = async () => {
+            try {
+                const res = await fetch("/api/clientes");
+                const data = await res.json();
+                setClientes(data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        fetchClientes();
+    }, []);
     return ( 
       <select
       id="nombre_cliente"
@@ -18,7 +24,7 @@ export const SelectNombreClienteTodos = async () => {
       className="mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
       >
           <option value={""}>Seleccione una opción</option>
-          {clienteNombre.map((cliente: any) => (
+          {clientes.map((cliente: any) => (
           <option key={cliente.nombre_cliente} value={cliente.nombre_cliente}>{cliente.nombre_cliente}</option>   
       ))}
       </select>   
