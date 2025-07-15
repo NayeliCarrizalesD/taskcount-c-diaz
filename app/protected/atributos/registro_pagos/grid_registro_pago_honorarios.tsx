8981af7bc8900b3ee119f91f32f9879fe7b7c903
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-07-15 10:07:06 -0600

    probando cosas

--- app/protected/atributos/registro_pagos/grid_registro_pago_honorarios.tsx
+++ app/protected/atributos/registro_pagos/grid_registro_pago_honorarios.tsx
@@ -1,19 +1,33 @@
+import { getPagosTodos } from "@/app/schema";
 import Footer from "../footer";
 import FormularioRegistroPagoHonorarios from "./place_formulario_pagos";
 import PlaceholderTablaPagosHonorarios  from "./place_tabla_registro_pagos_honorarios";
 
-
+import { useEffect, useState } from "react";
 
 export const GridRegistroPagoHonorarios = () => {
+    const [datosTabla, setDatosTabla] = useState<any[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await fetch("/api/clientes"); // Cambia aquí a tu API route
+                const datos = await res.json();
+                setDatosTabla(datos);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        fetchData();
+    }, []);
+
     return (
         <>
             <div className="px-4 grid gap-3 grid-cols-12">
                 <FormularioRegistroPagoHonorarios />
-                <PlaceholderTablaPagosHonorarios datosTabla={[]} />
-        
+                <PlaceholderTablaPagosHonorarios datosTabla={datosTabla} />
             </div>
             <Footer />
         </>
-
     );
 };
