aab72c8097a46edfad8415844cf6210af7e345b2
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-07-15 10:33:32 -0600

    error menso que no me di cuenta por menso xd

--- app/protected/atributos/registro_datos_usuario/place_formulario_datos_usuario.tsx
+++ app/protected/atributos/registro_datos_usuario/place_formulario_datos_usuario.tsx
@@ -1,10 +1,26 @@
 import { FormDatosUsuario } from "@/app/formularios/formDatosUsuario";
 import { SubmitButton } from "@/app/submit-button";
+import { createDatosUsuario, getUsuario } from "@/app/schema";
 import { FiUser } from "react-icons/fi";
-import { createRegistroPagoHonorarios } from "./funcion_registro_pago";
+import { redirect } from "next/navigation";
 
 export default function FormularioDatosUsuario() {
-  
+  async function datosUsuario(formData: FormData) {
+    'use server';
+    let fecha_alta = formData.get('fecha_alta') as unknown as string;
+    let telefono_usuario = formData.get('telefono_usuario') as string;
+    let correo = formData.get('correo') as string;
+    let nivel = formData.get('nivel') as string;
+    let usuario = await getUsuario(correo);
+
+    if (usuario.length > 0) {
+      return console.log('Usuario ya existe');  
+    }
+    else {
+        await createDatosUsuario(fecha_alta, telefono_usuario, correo, nivel);
+        redirect('/ProtectedRegistroDatosUsuarios');
+    }
+    }
     return (
         <div className="lg:col-span-4 sm:col-span-12 overflow-hidden rounded-3xl bg-slate-800 shadow-xl h-[auto] sm:h-auto overflow-y-scroll scrollbar-thin">
             <div className="p-4">
@@ -17,7 +33,7 @@ export default function FormularioDatosUsuario() {
                 </h3>
             </div>
             <div className="h-[auto] sm:h-auto px-4 flex-row items-center ">
-                <FormDatosUsuario action={createRegistroPagoHonorarios}>
+                <FormDatosUsuario action={datosUsuario}>
                     <SubmitButton>Registrar</SubmitButton>
                 </FormDatosUsuario>
             </div>
