2b7d813f0de6997122fa563229c3386ad691354f
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-07-16 17:00:02 -0600

    estilo

--- app/protected/atributos/registro_pagos/tabla_registro_pago.tsx
+++ app/protected/atributos/registro_pagos/tabla_registro_pago.tsx
@@ -25,7 +25,7 @@ export default function TablaPagosHonorarios({
     return (
         <>
             <div className="relative overflow-x-auto shadow-md sm:rounded-lg w-full h-full overflow-scroll rounded-lg bg-clip-border bg-zinc-900 my-5">
-        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 bg-neutral-800 dark:text-gray-400 table-auto min-w-max">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 bg-neutral-800 table-auto min-w-max">
           <thead>
             <tr>
               <th className='p-4 border-b border-neutral-500 text-slate-100 bg-zinc-900'>Nombre del Cliente</th>
