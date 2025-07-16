fc4a917758d38ca07ccb61281a79ea64e51d22ce
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-07-16 10:47:27 -0600

    prueba 3

--- app/protected/atributos/registro_pagos/place_tabla_registro_pagos_honorarios.tsx
+++ app/protected/atributos/registro_pagos/place_tabla_registro_pagos_honorarios.tsx
@@ -1,6 +1,6 @@
 "use client";
-//import { SelectClienteOnChange } from "../selectClienteFuncion";
-//import TablaPagosHonorarios from "./tabla_registro_pago";
+import { SelectClienteOnChange } from "../selectClienteFuncion";
+import TablaPagosHonorarios from "./tabla_registro_pago";
 
 import { useState } from 'react';
 
@@ -18,14 +18,14 @@ export default function PlaceholderTablaPagosHonorarios({ datosTabla }: {datosTa
                 </h3>
             </div>
             <div className="h-[auto] sm:h-auto px-4 flex-row items-center ">
-                {/*<SelectClienteOnChange 
+                <SelectClienteOnChange 
                   clienteSeleccionado={clienteSeleccionado}
                   setClienteSeleccionado={setClienteSeleccionado}
                 />
                 <TablaPagosHonorarios
                   clienteSeleccionado={clienteSeleccionado}
                   datosTabla={datosTabla} 
-                />*/}
+                />
             </div>
         </div>
     )
