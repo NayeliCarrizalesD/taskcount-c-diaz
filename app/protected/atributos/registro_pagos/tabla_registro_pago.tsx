e16cde8eb1e25022b284ad03e1ea5415d8e15ef2
NayeliCarrizalesDm <nayeli.carrizales.diaz@gmail.com>
2025-07-21 22:04:54 -0600

    paginacion

--- app/protected/atributos/registro_pagos/tabla_registro_pago.tsx
+++ app/protected/atributos/registro_pagos/tabla_registro_pago.tsx
@@ -88,7 +88,11 @@ const meses = [
             {Array.from({ length: totalPaginas }, (_, i) => (
               <li key={i + 1}>
                 <button
-                  className={`flex items-center justify-center px-3 h-8 leading-tight border border-gray-300 ${paginaActual === i + 1 ? "bg-blue-50 text-blue-600" : "bg-white text-gray-500 hover:bg-gray-100 hover:text-gray-700"} dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white`}
+                  className={`flex items-center justify-center px-3 h-8 leading-tight border border-gray-300 ${
+                    paginaActual === i + 1 
+                      ? "bg-blue-600 text-white border-blue-600 dark:bg-blue-500 dark:border-blue-500" 
+                      : "bg-white text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                  }`}
                   onClick={() => setPaginaActual(i + 1)}
                 >
                   {i + 1}
