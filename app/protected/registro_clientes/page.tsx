f3772f9f62f4e86e4946b85939625635741d3fd7
CryptoCapi <pnombre26@gmail.com>
2025-08-01 14:57:03 -0600

    mejoras

--- app/protected/registro_clientes/page.tsx
+++ app/protected/registro_clientes/page.tsx
@@ -1,39 +1,36 @@
-import {SideBarAdmin} from "../atributos/sidebar/sidebar_admin/sidebar";
-import { auth } from 'app/auth';
-import { getUsuario } from "@/app/schema";
+import { SideBarAdmin } from "../atributos/sidebar/sidebar_admin/sidebar";
 import { SideBarN1 } from "../atributos/sidebar/sidebar_nivel1/sidebar_n1";
 import { SideBarN2 } from "../atributos/sidebar/sidebar_nivel2/sidebar_n2";
 import { SideBarN3 } from "../atributos/sidebar/sidebar_nivel3/sidebar_n3";
 import { RegistroClientes } from "../atributos/registro_clientes/registro_cliente";
+import { auth } from "app/auth";
+import { getUsuario } from "@/app/schema";
 
 export default async function ProtectedRegistroDatosUsuarios() {
-  let session = await auth();
-    let correo = session?.user?.email;
-    let usuarios: any[] = [];
-    let nivelUsuario: string | undefined;
-    
-    try {
-      correo?.toString();
-      if (correo) {
-        const usuarioResponse = await getUsuario(correo);
-        usuarios = usuarioResponse;
-  
-        if (usuarios.length > 0) {
-          nivelUsuario = usuarios[0].nivel; // Asignar el nivel del primer usuario a la variable
-        }
+  const session = await auth();
+  const correo = session?.user?.email;
+  let nivelUsuario: string | undefined;
+
+  try {
+    if (correo) {
+      const usuarios = await getUsuario(correo);
+
+      if (usuarios.length > 0) {
+        nivelUsuario = usuarios[0].nivel; // Nivel del usuario
       }
     }
-    catch (error) {
-      console.error(error);
-    }
-    return (
-      
-      <main >
-        {nivelUsuario ==='na1' ?<SideBarAdmin/>: "" }
-        {nivelUsuario ==='n1' ?<SideBarN1/>: "" }
-        {nivelUsuario ==='n2' ?<SideBarN2/>: "" }
-        {nivelUsuario ==='n3' ?<SideBarN3/>: "" }
-        <RegistroClientes/>
-      </main>   
-    );
+  } catch (error) {
+    console.error("Error obteniendo usuario:", error);
   }
+
+  return (
+    <main>
+      {nivelUsuario === "na1" && <SideBarAdmin />}
+      {nivelUsuario === "n1" && <SideBarN1 />}
+      {nivelUsuario === "n2" && <SideBarN2 />}
+      {nivelUsuario === "n3" && <SideBarN3 />}
+      
+      <RegistroClientes />
+    </main>
+  );
+}
