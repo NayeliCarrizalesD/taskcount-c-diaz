45812a50a5415f85a21c3cc818b64146192a3e25
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-08-04 10:45:07 -0600

    Update button style in BtnEditar component for consistent rounded appearance

--- app/protected/atributos/BtnEditar.tsx
+++ app/protected/atributos/BtnEditar.tsx
@@ -40,9 +40,10 @@ export function BtnEditar({ onClick, cliente }: { onClick: (data: any) => void, 
       onClick={handleEditar}
       className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800"
     >
-        <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
+        <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-full group-hover:bg-transparent group-hover:dark:bg-transparent">
 Editar
 </span>
+
       
     </button>
   );
