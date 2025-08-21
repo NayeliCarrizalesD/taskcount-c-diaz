44fd90939da351e4708f6ab367e670ffa106faad
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-08-21 11:39:16 -0600

    Refactor fetchData function and update onPagoRegistrado prop in FormularioRegistroPagoHonorarios for improved data handling

--- app/protected/atributos/registro_pagos/grid_registro_pago_honorarios.tsx
+++ app/protected/atributos/registro_pagos/grid_registro_pago_honorarios.tsx
@@ -2,12 +2,12 @@
 import Footer from "../footer";
 import FormularioRegistroPagoHonorarios from "./place_formulario_pagos";
 import PlaceholderTablaPagosHonorarios  from "./place_tabla_registro_pagos_honorarios";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export const GridRegistroPagoHonorarios = () => {
     const [datosTabla, setDatosTabla] = useState<any[]>([]);
 
-    /*const fetchData = async () => {
+    const fetchData = async () => {
         try {
             const res = await fetch("/api/clientes-nombres");
             if (!res.ok) {
@@ -19,9 +19,9 @@ export const GridRegistroPagoHonorarios = () => {
             console.error('Error fetching data:', error);
             setDatosTabla([]); // Set empty array as fallback
         }
-    };*/
+    };
 
-    const fetchDatos = async () => {
+ /*   const fetchDatos = async () => {
   const res = await fetch("/api/pagos-honorarios");
   const data = await res.json();
   setDatosTabla(data);
@@ -29,12 +29,12 @@ export const GridRegistroPagoHonorarios = () => {
 
     useEffect(() => {
         fetchDatos();
-    }, []);
+    }, []);*/
 
     return (
         <>
             <div className="px-4 grid gap-3 grid-cols-12">
-                <FormularioRegistroPagoHonorarios onPagoRegistrado={fetchDatos} />
+                <FormularioRegistroPagoHonorarios onPagoRegistrado={fetchData} />
                 <PlaceholderTablaPagosHonorarios datosTabla={datosTabla} />
             </div>
             <Footer />
