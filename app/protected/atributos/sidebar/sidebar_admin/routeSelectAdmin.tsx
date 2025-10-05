d8e9ff6a3075b137f16272516381e7e330652a32
NayeliCarrizalesDm <nayeli.carrizales.diaz@gmail.com>
2025-10-05 02:12:04 -0600

    Update TransitionLink styles for improved visual consistency and feedback

--- app/protected/atributos/sidebar/sidebar_admin/routeSelectAdmin.tsx
+++ app/protected/atributos/sidebar/sidebar_admin/routeSelectAdmin.tsx
@@ -42,9 +42,9 @@ const TransitionLink = ({
     <li>
       <Link
         href={href}
-        className={`flex items-center p-2 rounded-full group transition-colors duration-200 ${isActive
-            ? "bg-blue-600 text-white dark:bg-blue-500"
-            : "text-gray-900 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+        className={`flex items-center p-2 rounded-xl group transition-colors duration-200 ${isActive
+            ? "bg-zinc-700 text-white dark:bg-zinc-700"
+          : "text-gray-900 dark:text-gray-900 hover:bg-sky-200 dark:hover:bg-sky-200"
           }`}
       >
         <Icon className={`transition-colors duration-200 ${isActive ? "text-white" : ""}`} />
