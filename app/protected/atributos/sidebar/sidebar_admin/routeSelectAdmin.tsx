204c4a7182780f7fc548998b6e893c1b65282586
NayeliCarrizalesDm <nayeli.carrizales.diaz@gmail.com>
2025-10-05 02:33:00 -0600

    Update TransitionLink styles for improved left margin consistency

--- app/protected/atributos/sidebar/sidebar_admin/routeSelectAdmin.tsx
+++ app/protected/atributos/sidebar/sidebar_admin/routeSelectAdmin.tsx
@@ -43,7 +43,7 @@ const TransitionLink = ({
       <Link
         href={href}
         className={`flex items-center p-3 group transition-all duration-300 relative ${isActive
-          ? "bg-zinc-700 text-white rounded-l-3xl shadow-xl ml-0"
+          ? "bg-zinc-700 text-white rounded-l-3xl shadow-xl ml-2 mr-0"
             : "text-white hover:bg-white hover:text-black rounded-full hover:ml-1"
           }`}
       >
