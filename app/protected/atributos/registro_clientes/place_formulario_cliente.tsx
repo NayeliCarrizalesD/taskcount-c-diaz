650e10e4668e5b139bcecfad38ce6cbe3b2a0519
NayeliCarrizalesDm <nayeli.carrizales.diaz@gmail.com>
2026-07-07 20:29:16 -0600

    feat: add client registration form component with server action to handle database creation

--- app/protected/atributos/registro_clientes/place_formulario_cliente.tsx
+++ app/protected/atributos/registro_clientes/place_formulario_cliente.tsx
@@ -28,7 +28,7 @@ export default function FormularioClientes() {
     }
 
     return (
-        <div className="lg:col-span-4 sm:col-span-12 overflow-hidden rounded-3xl bg-slate-800 shadow-xl h-[auto] sm:h-auto overflow-y-scroll scrollbar-thin">
+        <div className="lg:col-span-4 sm:col-span-12 overflow-hidden rounded-3xl bg-slate-800 shadow-xl self-start h-fit pb-4">
             <div className="p-4">
                 <h3 className="flex items-center text-lg gap-1.5 font-medium">
                     <FiFolder />
@@ -38,7 +38,7 @@ export default function FormularioClientes() {
                     Registrar un Cliente
                 </h3>
             </div>
-            <div className="h-[auto] sm:h-auto px-4 flex-row items-center ">
+            <div className="px-4 flex-row items-center">
                 <FormRegistroCliente action={RegistroCatalogoCliente}>
                     <SubmitButton>Registrar Cliente </SubmitButton>
                 </FormRegistroCliente>
